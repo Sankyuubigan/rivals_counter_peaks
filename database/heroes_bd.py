fbe3fa8b4adb16017897312a027ae73adc43841b
Nilden <nilden@yandex.ru>
2025-05-09 01:03:34 +0300

    Внесены некоторые изменения в базу данных.

--- database/heroes_bd.py
+++ database/heroes_bd.py
@@ -1,98 +1,174 @@
 # Словарь контрпиков: ключ - герой, значение - список тех, кого он контрит
 heroes_counters = {
-    "Hulk": ["Iron Man", "Psylocke", "Storm", "Punisher", "Namor", "Thor", "Peni Parker", "Wolverine", "Mantis",
-             "Luna Snow", "Bucky", "Adam Warlock","Emma Frost","Moon Knight"],
-    "Groot": ["Punisher", "Hulk", "Venom", "Storm", "Wolverine", "Iron Man", "Bucky", "Hela", "Cloak and Dagger",
+    "Hulk": {
+        "hard":[],
+        "soft":["Iron Man", "Psylocke", "Storm", "Punisher", "Namor", "Thor", "Peni Parker", "Wolverine", "Mantis",
+             "Luna Snow", "Bucky", "Adam Warlock","Emma Frost","Moon Knight"]
+    },
+    "Groot": {
+        "hard":[],
+        "soft":["Punisher", "Hulk", "Venom", "Storm", "Wolverine", "Iron Man", "Bucky", "Hela", "Cloak and Dagger",
               "Captain America", "Thor", "Rocket Racoon", "Luna Snow", "Mantis", "Invisible Woman", "Psylocke",
-              "StarLord", "SpiderMan", "Emma Frost","Moon Knight","Human Torch"],
-    "Thor": ["Peni Parker", "Iron Man", "Storm", "Wolverine", "Punisher", "StarLord", "Witch", "Mantis",
+              "StarLord", "SpiderMan", "Emma Frost","Moon Knight","Human Torch"]},
+    "Thor": {
+        "hard":[],
+        "soft":["Peni Parker", "Iron Man", "Storm", "Wolverine", "Punisher", "StarLord", "Witch", "Mantis",
              "Psylocke", "Bucky", "Luna Snow",
-             "Adam Warlock", "Witch", "Namor", "Squirrel Girl","Emma Frost","Moon Knight","Human Torch"],
-    "Doctor Strange": ["Wolverine", "Black Panther", "Hulk", "Magik", "Punisher", "Psylocke",
-                       "Invisible Woman", "Mantis", "Luna Snow", "Mr Fantastic", "Bucky"],
-    "The Thing": ["Groot", "Iron Man", "Squirrel Girl", "Storm", "Hela", "Adam Warlock", "Witch", "Moon Knight",
+             "Adam Warlock", "Witch", "Namor", "Squirrel Girl","Emma Frost","Moon Knight","Human Torch"]},
+    "Doctor Strange": {
+        "hard":[],
+        "soft":["Wolverine", "Black Panther", "Hulk", "Magik", "Punisher", "Psylocke",
+                       "Invisible Woman", "Mantis", "Luna Snow", "Mr Fantastic", "Bucky"]},
+    "The Thing": {
+        "hard":[],
+        "soft":["Groot", "Iron Man", "Squirrel Girl", "Storm", "Hela", "Adam Warlock", "Witch", "Moon Knight",
                   "Doctor Strange", "StarLord", "Peni Parker", "Punisher", "Psylocke", "Hawkeye",
-                  "Widow", "Namor", "Human Torch", "Loki", "Jeff", "Rocket Racoon", "Cloak and Dagger","Emma Frost"],
-    "Venom": ["Wolverine", "Punisher", "Storm", "Iron Man", "Bucky", "Peni Parker", "Adam Warlock",
-              "Witch", "Squirrel Girl", "Namor", "Luna Snow", "Mantis", "The Thing","Emma Frost","Human Torch"],
-    "Peni Parker": ["Iron Man", "Storm", "Punisher", "StarLord", "Doctor Strange", "Namor", "Groot", "Loki",
-                    "Bucky", "Hela", "Moon Knight", "Psylocke", "Adam Warlock", "Invisible Woman","Human Torch"],
-    "Captain America": ["Groot", "Namor", "Hela", "Storm", "Witch", "Loki", "Peni Parker", "Iron Man",
-                        "Human Torch", "Jeff", "Rocket Racoon", "Adam Warlock", "Witch", "The Thing","Emma Frost"],
-    "Magneto": ["Storm", "Groot", "Magik", "Loki", "Adam Warlock",
-                "Luna Snow", "Mr Fantastic", "Bucky", "SpiderMan", "The Thing"],
-    "Emma Frost": ["Punisher", "Hela",  "Hawkeye", "Widow","Human Torch","Iron Man","Storm","Witch"],
+                  "Widow", "Namor", "Human Torch", "Loki", "Jeff", "Rocket Racoon", "Cloak and Dagger","Emma Frost"]},
+    "Venom": {
+        "hard":[],
+        "soft":["Wolverine", "Punisher", "Storm", "Iron Man", "Bucky", "Peni Parker", "Adam Warlock",
+              "Witch", "Squirrel Girl", "Namor", "Luna Snow", "Mantis", "The Thing","Emma Frost","Human Torch"]},
+    "Peni Parker": {
+        "hard":["Iron Man", "Storm", "Namor","Punisher", "Bucky","StarLord","Widow","Moon Knight","Hawkeye","Squirrel Girl","Human Torch","Hela","Psylocke",],
+        "soft":[ "Doctor Strange",  "Groot",  "Adam Warlock", "Invisible Woman",
+                    "Magneto","Wolverine","Witch","Luna Snow","Jeff","Mantis","Cloak and Dagger"]},
+    "Captain America": {
+        "hard":[],
+        "soft":["Groot", "Namor", "Hela", "Storm", "Witch", "Loki", "Peni Parker", "Iron Man",
+                        "Human Torch", "Jeff", "Rocket Racoon", "Adam Warlock", "Witch", "The Thing","Emma Frost"]},
+    "Magneto": {
+        "hard":[],
+        "soft":["Storm", "Groot", "Magik", "Loki", "Adam Warlock",
+                "Luna Snow", "Mr Fantastic", "Bucky", "SpiderMan", "The Thing"]},
+    "Emma Frost": {
+        "hard":[],
+        "soft":["Punisher", "Hela",  "Hawkeye", "Widow","Human Torch","Iron Man","Storm","Witch"]},
 
 
-    "Punisher": ["Hela", "Hawkeye", "Fister", "Magik", "Black Panther",
-                 "Magneto", "Moon Knight", "Loki","Squirrel Girl"],
-    "Hela": ["Punisher", "Magik", "Doctor Strange", "Luna Snow", "Loki", "Venom", "Captain America", "Black Panther",
-             "Invisible Woman", "Rocket Racoon"],
-    "Moon Knight": ["Storm", "Black Panther", "Iron Man", "SpiderMan", "Magneto", "Psylocke", "Magik",
-                    "Venom", "StarLord", "Captain America", "Thor", "Hulk"],
-    "Iron Man": [
-        "Doctor Strange", "Fister", "Rocket Racoon", "Namor", "StarLord",
-        "Punisher", "Psylocke", "SpiderMan", "Widow", "Hela", "Hawkeye", "Adam Warlock", "Mantis", "Luna Snow",
+    "Punisher": {
+        "hard":[],
+        "soft":["Hela", "Hawkeye", "Fister", "Magik", "Black Panther",
+                 "Magneto", "Moon Knight", "Loki","Squirrel Girl"]},
+    "Hela": {
+        "hard":[],
+        "soft":["Punisher", "Doctor Strange", "Luna Snow", "Venom", "Captain America", "Black Panther",
+             "Invisible Woman","Psylocke",
+                "Adam Warlock","Cloak and Dagger", "Jeff"]},
+    "Moon Knight": {
+        "hard":[],
+        "soft":["Storm", "Black Panther", "Iron Man", "SpiderMan", "Magneto", "Psylocke", "Magik",
+                    "Venom", "StarLord", "Captain America", "Thor", "Hulk"]},
+    "Iron Man": {
+        "hard":["Doctor Strange", "Fister", "Namor","Punisher",  "Widow", "Hela", "Hawkeye","SpiderMan",],
+        "soft":[
+         "StarLord",
+        "Psylocke",  "Adam Warlock", "Mantis", "Luna Snow",
         "Hulk", "Witch", "Bucky"
-    ],
-    "Bucky": ["Hela", "Hawkeye", "Namor", "Fister"],
-    "Psylocke": ["StarLord", "Invisible Woman", "Namor", "Punisher", "Luna Snow", "Mantis",
-                 "Captain America", "Magneto", "Rocket Racoon", "Loki", "Cloak and Dagger"],
-    "Mr Fantastic": ["Groot", "Storm", "Punisher", "Iron Man", "Hawkeye", "Hela", "Bucky", "Widow",
-                      "Squirrel Girl", "Mantis", "Luna Snow", "Adam Warlock", "Cloak and Dagger", "Jeff", "Peni Parker",
-                      "Witch","Emma Frost"],
-    "Storm": [
+    ]},
+    "Bucky": {
+        "hard":[],
+        "soft":["Hela", "Hawkeye", "Namor", "Fister","Squirrel Girl","Widow","Moon Knight"]},
+    "Psylocke": {
+        "hard":[],
+        "soft":["StarLord", "Invisible Woman", "Namor", "Punisher", "Luna Snow", "Mantis",
+                 "Captain America", "Magneto", "Rocket Racoon", "Loki", "Cloak and Dagger"]},
+    "Mr Fantastic": {
+        "hard":["Punisher", "Hawkeye", "Hela", "Widow","Squirrel Girl","Moon Knight","Bucky","StarLord"],
+        "soft":["Groot", "Storm", "Iron Man",
+                       "Mantis", "Luna Snow", "Adam Warlock", "Cloak and Dagger", "Jeff", "Peni Parker",
+                      "Witch","Emma Frost"]},
+    "Storm": {
+        "hard":[],
+        "soft":[
         "Doctor Strange", "Punisher", "Hela", "Namor", "Fister", "Psylocke",
         "Widow", "SpiderMan", "StarLord",
         "Cloak and Dagger", "Hulk", "Witch", "Bucky"
-    ],
-    "Black Panther": [
+    ]},
+    "Black Panther":{
+        "hard":[],
+        "soft": [
         "The Thing", "Human Torch", "StarLord", "Peni Parker", "Iron Man", "Storm", "Namor", "Cloak and Dagger",
         "Magneto", "Punisher", "Psylocke", "Mantis", "Bucky", "Jeff", "Adam Warlock", "Luna Snow", "Rocket Racoon", "Witch"
-    ],
-    "Squirrel Girl": ["Groot", "Captain America", "Hawkeye", "Storm", "StarLord",
-                      "Iron Man", "Witch", "Human Torch", "Magneto", "Hulk", "Hela", "Widow"],
-    "SpiderMan": ["Namor", "Hela", "Hawkeye", "Luna Snow", "Cloak and Dagger",
+    ]},
+    "Squirrel Girl": {
+        "hard":[],
+        "soft":["Groot", "Captain America", "Hawkeye", "Storm", "StarLord",
+                      "Iron Man", "Witch", "Human Torch", "Magneto", "Hulk", "Hela", "Widow"]},
+    "SpiderMan": {
+        "hard":[],
+        "soft":["Namor", "Hela", "Hawkeye", "Luna Snow", "Cloak and Dagger",
                    "Adam Warlock", "Rocket Racoon", "Jeff",
-                   "Squirrel Girl", "Peni Parker", "Captain America", "Witch", "Bucky", "Punisher","The Thing","Emma Frost"],
-    "StarLord": ["Magneto", "Luna Snow", "Loki", "Doctor Strange", "Hela", "Punisher", "Hawkeye", "Namor", "Mantis",
-                  "Invisible Woman", "Hulk", "Witch"],
-    "Wolverine": ["Peni Parker", "Iron Man", "Namor", "Storm", "Invisible Woman", "Captain America", "Psylocke",
+                   "Squirrel Girl", "Peni Parker", "Captain America", "Witch", "Bucky", "Punisher","The Thing","Emma Frost"]},
+    "StarLord": {
+        "hard":["Punisher", ],
+        "soft":["Magneto", "Luna Snow", "Loki", "Doctor Strange", "Mantis","Hela", "Hawkeye",
+                  "Invisible Woman", "Hulk", "Witch"]},
+    "Wolverine": {
+        "hard":[],
+        "soft":["Peni Parker", "Iron Man", "Namor", "Storm", "Invisible Woman", "Captain America", "Psylocke",
                   "Mantis", "Luna Snow", "Magneto", "Hela", "Adam Warlock", "Witch", "The Thing",
-                  "Cloak and Dagger","Mr Fantastic"],
-    "Hawkeye": ["Psylocke", "Groot", "SpiderMan", "Hela", "Doctor Strange", "Black Panther", "Fister",
-                "Rocket Racoon", "Loki"],
-    "Magik": ["Iron Man", "Namor", "Storm", "Doctor Strange", "Luna Snow", "Peni Parker", "Magneto", "Thor",
-              "Bucky", "Adam Warlock", "Cloak and Dagger", "Jeff", "Rocket Racoon", "Witch", "The Thing","Emma Frost","Human Torch"],
-    "Widow": ["Psylocke", "Magik", "Black Panther", "Doctor Strange", "Venom", "Captain America",
-                    "Witch"],
-    "Witch": ["Punisher", "Hawkeye", "Hela", "Peni Parker", "Mantis", "Luna Snow"],
-    "Namor": ["Moon Knight", "Hela", "Hawkeye", "Magneto", "Doctor Strange", "Punisher", "Bucky",
-              "Loki", "Cloak and Dagger", "Adam Warlock"],
-    "Fister": ["Namor", "Captain America", "Invisible Woman", "Rocket Racoon", "Loki", "Storm",
-                  "Luna Snow", "Bucky", "Punisher", "Mantis", "Hulk", "Peni Parker", "Witch",
-                  "Squirrel Girl", "Magik", "Hela", "Adam Warlock","Emma Frost"],
-    "Human Torch": ["Hela", "Hawkeye", "Punisher", "Psylocke", "SpiderMan", "Luna Snow", "Hulk", "Witch"],
+                  "Cloak and Dagger","Mr Fantastic"]},
+    "Hawkeye": {
+        "hard":["Black Panther", "Psylocke", "Widow","Squirrel Girl","Bucky","Moon Knight"],
+        "soft":[ "Groot", "SpiderMan", "Hela", "Doctor Strange", "Fister", "Jeff","Invisible Woman",
+                 "Witch","StarLord", "Cloak and Dagger", "Luna Snow"]},
+    "Magik": {
+        "hard":["Namor", "Iron Man", "Storm","Human Torch","The Thing","Magneto","Peni Parker", "Emma Frost",],
+        "soft":[  "Luna Snow",  "Thor","Doctor Strange",
+              "Bucky", "Adam Warlock", "Cloak and Dagger", "Jeff", "Rocket Racoon", "Witch", ]},
+    "Widow": {
+        "hard":[],
+        "soft":["Psylocke", "Magik", "Black Panther", "Doctor Strange", "Venom", "Captain America",
+                    "Witch"]},
+    "Witch": {
+        "hard":[],
+        "soft":["Punisher", "Hawkeye", "Hela", "Peni Parker", "Mantis", "Luna Snow"]},
+    "Namor": {
+        "hard":[],
+        "soft":["Moon Knight", "Hela", "Hawkeye", "Magneto", "Doctor Strange", "Punisher", "Bucky",
+              "Loki", "Cloak and Dagger", "Adam Warlock"]},
+    "Fister": {
+        "hard":["Namor","Adam Warlock","Invisible Woman","Punisher", "Peni Parker","Squirrel Girl","Emma Frost",],
+        "soft":[  "Storm", "Luna Snow", "Bucky", "Mantis", "Hulk",  "Witch","Magik", "Hela" ]},
+    "Human Torch": {
+        "hard":["Hela", "Hawkeye", "Punisher", "Psylocke", "SpiderMan","Widow","Bucky","Namor","StarLord"],
+        "soft":[ "Luna Snow", "Hulk", "Witch","Adam Warlock"]},
 
 
-    "Loki": ["Moon Knight", "Iron Man", "Punisher", "Bucky", "Wolverine", "Adam Warlock", "SpiderMan",
-             "Mr Fantastic", "Captain America", "Thor", "The Thing", "Storm", "Hela", "Jeff", "Moon Knight"],
-    "Cloak and Dagger": ["Magneto", "Magik", "Hela", "Hawkeye", "Psylocke", "Wolverine", "Jeff", "Squirrel Girl",
-                       "Moon Knight", "Rocket Racoon","Emma Frost","Witch","Iron Man"],
-    "Jeff": ["Iron Man", "Storm", "Psylocke", "Peni Parker", "Squirrel Girl", "Magneto", "Hela",
-             "Cloak and Dagger", "Fister", "Luna Snow", "Mantis", "The Thing", "Namor","Emma Frost"],
-    "Mantis": ["Iron Man", "Punisher", "Fister", "Captain America", "Psylocke", "Jeff", "Magneto", "Hela", "Magik",
+    "Loki": {
+        "hard":["Moon Knight","Punisher","Bucky", "Wolverine","Widow","Squirrel Girl"],
+        "soft":[   "Adam Warlock", "SpiderMan","Venom",
+             "Mr Fantastic", "Captain America", "Thor", "The Thing", "Storm", "Hela", "Jeff", "Moon Knight","Witch"]},
+    "Cloak and Dagger": {
+        "hard":["Widow","Squirrel Girl","Bucky","Moon Knight"],
+        "soft":["Magneto", "Magik", "Hela", "Hawkeye", "Psylocke", "Wolverine", "Jeff",
+                       "Rocket Racoon","Emma Frost","Witch","Iron Man"]},
+    "Jeff": {
+        "hard":[],
+        "soft":["Iron Man", "Storm", "Psylocke", "Peni Parker", "Squirrel Girl", "Magneto", "Hela",
+             "Cloak and Dagger", "Fister", "Luna Snow", "Mantis", "The Thing", "Namor","Emma Frost"]},
+    "Mantis": {
+        "hard":[],
+        "soft":["Iron Man", "Punisher", "Fister", "Captain America", "Psylocke", "Jeff", "Magneto", "Hela", "Magik",
                "Black Panther", "Moon Knight", "Squirrel Girl", "The Thing", "Wolverine", "StarLord", "Rocket Racoon",
-               "Storm","Witch"],
-    "Luna Snow": ["Iron Man", "Punisher", "Psylocke", "Witch", "Captain America", "Loki", "Magneto",
+               "Storm","Witch"]},
+    "Luna Snow": {
+        "hard":[],
+        "soft":["Iron Man", "Punisher", "Psylocke", "Witch", "Captain America", "Loki", "Magneto",
                   "The Thing", "Magik", "Black Panther", "Mr Fantastic", "Moon Knight", "Venom", "Thor",
-                  "Hulk", "Rocket Racoon", "Storm"],
-    "Rocket Racoon": ["StarLord", "Black Panther", "Iron Man", "Psylocke", "Venom", "Magneto", "Fister",
-                      "Captain America", "Punisher", "Wolverine","Witch"],
-    "Invisible Woman": ["Punisher", "Moon Knight", "Squirrel Girl", "Magik", "SpiderMan", "Jeff", "Iron Man",
-                        "Magneto", "Namor", "The Thing", "Rocket Racoon", "Mr Fantastic", "Storm", "Black Panther","Emma Frost","Witch"],
-    "Adam Warlock": ["Black Panther", "Magneto", "Doctor Strange", "Hela", "Widow",
-                     "Bucky", "Squirrel Girl", "Storm"],
+                  "Hulk", "Rocket Racoon", "Storm"]},
+    "Rocket Racoon": {
+        "hard":[],
+        "soft":["StarLord", "Black Panther", "Iron Man", "Psylocke", "Venom", "Magneto", "Fister",
+                      "Captain America", "Punisher", "Wolverine","Witch"]},
+    "Invisible Woman": {
+        "hard":[],
+        "soft":["Punisher", "Moon Knight", "Squirrel Girl", "Magik", "SpiderMan", "Jeff", "Iron Man",
+                        "Magneto", "Namor", "The Thing", "Rocket Racoon", "Mr Fantastic", "Storm", "Black Panther","Emma Frost","Witch"]},
+    "Adam Warlock": {
+        "hard":[],
+        "soft":["Black Panther", "Magneto", "Doctor Strange", "Hela", "Widow",
+                     "Bucky", "Squirrel Girl", "Storm"]},
 }
 
 # Основной список всех героев
@@ -142,7 +218,7 @@ heroes_compositions = {
     "Magik": [],
     "Widow": ["Hawkeye"],
     "Witch": ["Doctor Strange", "Jeff"],
-    "Namor": ["Luna Snow"],
+    "Namor": ["Hulk"],
     "Fister": [],
     "Human Torch": ["Invisible Woman"],
 
@@ -160,11 +236,13 @@ heroes_compositions = {
 
 
 # перепроверить нужно : = [
-#     "Peni Parker", "Hela",
-#     "Iron Man",  "Mr. Fantastic",
-#     "Star-Lord", "Hawkeye", "Magik", "Black Widow",
-#     "Iron Fist", "Human Torch", "Loki", "Cloak & Dagger", "Mantis", "Luna Snow",
+#
+#
+#
+#       "Luna Snow",
 #     "Rocket Racoon", "Invisible Woman", "Adam Warlock",
+
+
 # ]
 
 # https://rivalsmeta.com/characters/spider-man/matchups
