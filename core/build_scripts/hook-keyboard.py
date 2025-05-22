c80f7bb97ceacfdbbea1dea8739f615a24e407c7
Nilden <nilden@yandex.ru>
2025-05-22 12:08:43 +0300

    Добавлены новые задачи. Добавлены иконки для распознавания.

--- core/build_scripts/hook-keyboard.py
+++
@@ -1,3 +0,0 @@
-# File: core/build_scripts/hook-keyboard.py
-# Этот файл больше не используется, хук находится в build_scripts
-print("[INFO] core/build_scripts/hook-keyboard.py больше не используется.")
